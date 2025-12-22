de40927a92155cc3b36199ca6c0d40f743d38322
Smith-Faldu <smith.faldu.1108@gmail.com>
2025-12-22 10:04:47 +0530

    route issue reminaing

--- lib/app_routes.dart
+++
@@ -1,9 +0,0 @@
-class AppRoutes {
-  static const login = '/login';
-  static const registerOrg = '/register-org';
-
-  static const adminHome = '/admin';
-  static const managerHome = '/manager';
-  static const supervisorHome = '/supervisor';
-  static const labourHome = '/labour';
-}
